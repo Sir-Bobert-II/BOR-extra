efbf470e30ab8871eab80797c9eb0d0a4e6ec645
Decator <decator.c@proton.me>
2023-02-07 14:18:38 -0500

    start

---
+++ quote/src/lib.rs
@@ -0,0 +1,26 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Error,Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum QuoteError
+{
+    #[error("JSONParseError: couldn't parse json: {message}")]
+    JsonParse {message: String},
+
+    #[default]
+    #[error("Error: An unknown error occurred.")]
+    GeneralError,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RandomQuote {
+    #[serde(rename = "_id")]
+    pub id: String,
+    pub content: String,
+    pub author: String,
+    pub tags: Vec<String>,
+    pub author_slug: String,
+    pub length: i64,
+    pub date_added: String,
+    pub date_modified: String,
+}
